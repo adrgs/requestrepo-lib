891cf562edf4f1b0d5c1483a7352cf8aa4296d60
adrgs <albastroiudragos@gmail.com>
2024-04-19 21:04:18 +0200

    Add unit test for DNS API

---
+++ tests/dns.py
@@ -0,0 +1,23 @@
+import unittest
+import requestrepo
+import requests
+import socket
+
+class TestRequest(unittest.TestCase):
+    def test_add_dns(self):
+      r = requestrepo.RequestRepo()
+
+      r.add_dns("testcanary1", "A", "1.1.1.1")
+
+      ip = socket.gethostbyname(f"testcanary1.{r.domain}")
+
+      self.assertEqual(ip, "1.1.1.1")
+
+      r.add_dns("testcanary1", "A", "8.8.8.8")
+
+      ip = socket.gethostbyname(f"{r.domain}")
+
+      self.assertNotEqual(ip, "8.8.8.8")
+
+if __name__ == '__main__':
+  unittest.main()
