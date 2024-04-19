af13390f5a47f06627453967df3b6924c449ff7d
adrgs <albastroiudragos@gmail.com>
2024-04-19 20:43:51 +0200

    Add unit test for modifying response

---
+++ tests/response.py
@@ -0,0 +1,43 @@
+import unittest
+import requestrepo
+import requests
+
+class TestResponse(unittest.TestCase):
+    def test_update_raw(self):
+        r = requestrepo.RequestRepo()
+
+        raw = b'Hello world!'
+
+        r.update_response(raw=raw)
+        data = r.response()
+
+        t = requests.get(f"http://{r.domain}/test")
+
+        self.assertEqual(data["raw"], raw)
+        self.assertEqual(data["raw"].decode(), t.text)
+
+
+    def test_update_headers(self):
+      r = requestrepo.RequestRepo()
+
+      headers = {"test": "test"}
+
+      r.update_response(headers=headers)
+      data = r.response()
+
+      self.assertEqual(data["headers"], headers)
+
+
+    def test_update_status_code(self):
+      r = requestrepo.RequestRepo()
+
+      status_code = 404
+
+      r.update_response(status_code=status_code)
+      data = r.response()
+
+      self.assertEqual(data["status_code"], status_code)
+
+
+if __name__ == '__main__':
+  unittest.main()
