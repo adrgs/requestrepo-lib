266b2cb8f3691666165fd3387f92187146342963
adrgs <albastroiudragos@gmail.com>
2024-05-19 16:53:11 +0200

    Update response tests

--- tests/response.py
+++ tests/response.py
@@ -8,13 +8,13 @@ class TestResponse(unittest.TestCase):
 
         raw = b'Hello world!'
 
-        r.update_response(raw=raw)
+        r.update_http(raw=raw)
         data = r.response()
 
         t = requests.get(f"http://{r.domain}/test")
 
-        self.assertEqual(data["raw"], raw)
-        self.assertEqual(data["raw"].decode(), t.text)
+        self.assertEqual(data.raw, raw)
+        self.assertEqual(data.raw.decode(), t.text)
 
 
     def test_update_headers(self):
@@ -22,10 +22,10 @@ class TestResponse(unittest.TestCase):
 
       headers = {"test": "test"}
 
-      r.update_response(headers=headers)
+      r.update_http(headers=headers)
       data = r.response()
 
-      self.assertEqual(data["headers"], headers)
+      self.assertEqual(data.headers, headers)
 
 
     def test_update_status_code(self):
@@ -33,10 +33,10 @@ class TestResponse(unittest.TestCase):
 
       status_code = 404
 
-      r.update_response(status_code=status_code)
+      r.update_http(status_code=status_code)
       data = r.response()
 
-      self.assertEqual(data["status_code"], status_code)
+      self.assertEqual(data.status_code, status_code)
 
 
 if __name__ == '__main__':
